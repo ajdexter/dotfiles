e5686d197aed0136616d5408e1089eda620aedad
AJ Dexter <aj.dexter@gmail.com>
2019-01-05 09:50:36 -0800

    repair gittower and exa aliases
    Signed-off-by: AJ Dexter <aj.dexter@gmail.com>

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,13 @@
 #!/bin/sh
 alias reload!='exec "$SHELL" -l'
+
+# App
+alias to.='gittower .' # Open current dir in Tower
+
+# Replace 'ls' with exa if it is available.
+if command -v exa >/dev/null 2>&1; then
+    alias ls="exa --git --color=automatic"
+    alias ll="exa --all --long --git --color=automatic"
+    alias la="exa --all --binary --group --header --long --git --color=automatic"
+    alias l="exa --git --color=automatic"
+fi
